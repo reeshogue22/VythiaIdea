5a0db1f108fb97ce430b1b9e8c42f179374514d9
Larson Carter <larsoncarter72404@gmail.com>
2024-02-13 16:43:25 -0600

    Dev Staging
    I did some dev staging assisted by JetBrains PyCharm and GPT

--- train_encoder.py
+++ train_encoder.py
@@ -1,26 +1,17 @@
 from gan import Generator
 from encoder import ImageEncoder
-
 import torch
 import torch.nn.functional as F
 import torch.utils.data.dataloader as D
 from dataset import Dataset
 from utils import *
 from sklearn.model_selection import train_test_split
-import atexit
 from tqdm import tqdm
 
 class Trainer:
-    def __init__(self, epochs=4,
-                       batch_size=8,
-                       lr=1e-4,
-                       embedding_size=32000,
-                       save_every=100,
-    ):
-        self.generator = Generator(latent_dim=embedding_size).to('mps')
-        gw = torch.load('engine.pt', map_location='mps')
-        self.generator.load_state_dict(gw)
-        self.encoder = ImageEncoder(latent_dim=embedding_size).to('mps')
+    def __init__(self, epochs=4, batch_size=8, lr=1e-4, embedding_size=32000, save_every=100):
+        self.generator = Generator(latent_dim=embedding_size).to(device)
+        self.encoder = ImageEncoder(latent_dim=embedding_size).to(device)
         self.optim = torch.optim.RAdam(self.encoder.parameters(), lr=lr)
         self.dataset = Dataset(max_ctx_length=0, data_aug=True)
         self.train_dataloader = D.DataLoader(self.dataset, shuffle=True, batch_size=batch_size)
@@ -28,15 +19,14 @@ class Trainer:
         self.save_every = save_every
         self.epochs = epochs
         self.embedding_size = embedding_size
+
     def train(self):
         """Train the model."""
         print("Beginning Training...")
-        self.encoder.train()
         for epoch in range(self.epochs):
             print("Starting Epoch:", epoch)
-            self.encoder.train()
-            bar = tqdm(range(len(self.train_dataloader)))
-            for i, (x) in enumerate(self.train_dataloader):
+            bar = tqdm(enumerate(self.train_dataloader))
+            for i, (x) in bar:
                 if not i % self.save_every == 0:
                     engine_loss = self.training_step(x)
                     bar.set_description(f'Loss: {engine_loss:.4f}')
@@ -45,31 +35,25 @@ class Trainer:
                 bar.update(1)
 
     def training_step(self, x):
-        #Get a batch of images
-        x = x.to('mps')
+        x = x.to(device)
         self.optim.zero_grad()
 
-        #FAKE DATA
-
-        #Generate a random latent vector
-        latent = torch.randint(0, self.embedding_size, (x.shape[0],), device=x.device)
-        #Generate a fake image
+        # FAKE DATA
+        latent = torch.randint(0, self.embedding_size, (x.shape[0],), device=device)
         fake = self.generator(torch.zeros_like(x), latent.unsqueeze(1))
-
-        #Encode the fake image
         fake_encoded = self.encoder(fake)
 
-        #Get the loss
         loss = F.cross_entropy(fake_encoded, latent)
         loss.backward()
         self.optim.step()
         return loss.item()
-    
+
     def save(self):
         torch.save(self.encoder.state_dict(), 'encoder.pt')
         print("Saved model.")
 
 if __name__ == "__main__":
+    device = torch.device('mps')  # Define the device
     trainer = Trainer()
     trainer.train()
     trainer.save()
--- train_gan.py
+++ train_gan.py
@@ -5,22 +5,16 @@ from gan import Generator, Discriminator
 from dataset import Dataset
 from utils import *
 from sklearn.model_selection import train_test_split
-import atexit
 from tqdm import tqdm
 
+
 class Trainer:
-    def __init__(self, epochs=4,
-                       batch_size=8,
-                       lr=1e-4,
-                       dlr=1e-5,
-                       save_every=100,
-                       embedding_size=32000
-            ):
+    def __init__(self, epochs=4, batch_size=8, lr=1e-4, dlr=1e-5, save_every=100, embedding_size=32000):
         self.epochs = epochs
         self.save_every = save_every
         self.embedding_size = embedding_size
-        self.engine = Generator(latent_dim=embedding_size).to('mps')
-        self.discriminator = Discriminator().to('mps')
+        self.engine = Generator(latent_dim=embedding_size).to(device)
+        self.discriminator = Discriminator().to(device)
         self.optim = torch.optim.RAdam(self.engine.parameters(), lr=lr)
         self.discriminator_optim = torch.optim.Adam(self.discriminator.parameters(), lr=dlr)
         print(get_nparams(self.engine), "params in generator net.")
@@ -30,42 +24,37 @@ class Trainer:
 
     def train(self):
         """Train the model."""
-
         print("Beginning Training...")
-        self.engine.train()
         for epoch in range(self.epochs):
             print("Starting Epoch:", epoch)
-            self.engine.train()
-            bar = tqdm(range(len(self.train_dataloader)))
-            for i, (x) in enumerate(self.train_dataloader):
+            bar = tqdm(enumerate(self.train_dataloader))
+            for i, (x) in bar:
                 if not i % self.save_every == 0:
                     engine_loss = self.training_step(x)
                     bar.set_description(f'Loss: {engine_loss:.4f}')
                 else:
                     self.save()
                 bar.update(1)
-    
+
     def training_step(self, x):
-        """
-        One optimization step
-        :param x: Input data
-        :param y: Target data
-        :param step: Current training step
-        :return: loss
-        """
-        x = x.to('mps')
+        x = x.to(device)
         self.optim.zero_grad()
-        x_hat = self.engine(torch.zeros_like(x), torch.randint(0, self.embedding_size, (x.shape[0], 1), device=x.device))
+
+        # Generator training
+        x_hat = self.engine(torch.zeros_like(x), torch.randint(0, self.embedding_size, (x.shape[0], 1), device=device))
         disc_false = self.discriminator(x_hat)
         loss = F.binary_cross_entropy_with_logits(disc_false, torch.ones_like(disc_false))
         loss.backward()
         self.optim.step()
 
+        # Discriminator training
         self.discriminator_optim.zero_grad()
         disc_true = self.discriminator(x)
-        x_hat = self.engine(torch.zeros_like(x), torch.randint(0, self.embedding_size, (x.shape[0], 1), device=x.device))
+        x_hat = self.engine(torch.zeros_like(x), torch.randint(0, self.embedding_size, (x.shape[0], 1), device=device))
         disc_false = self.discriminator(x_hat)
-        dloss = F.binary_cross_entropy_with_logits(disc_true, torch.ones_like(disc_true)) + F.binary_cross_entropy_with_logits(disc_false, torch.zeros_like(disc_false))
+        dloss = F.binary_cross_entropy_with_logits(disc_true,
+                                                   torch.ones_like(disc_true)) + F.binary_cross_entropy_with_logits(
+            disc_false, torch.zeros_like(disc_false))
         dloss.backward()
         self.discriminator_optim.step()
 
@@ -80,6 +69,7 @@ class Trainer:
 
 
 if __name__ == "__main__":
+    device = torch.device('mps')  # Define the device
     trainer = Trainer()
     atexit.register(trainer.save)
     trainer.train()
